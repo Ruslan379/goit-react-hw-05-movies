29a26057d23b85c383555e713d00f452037995b8
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-09-06 17:20:47 +0300

    Test Konspekt - Индексные маршруты

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,76 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Home } from "pages/Home";
 
-import { About } from "pages/About";
-  import { Mission } from "components/Mission";
-  import { Team } from "components/Team";
-  import { Reviews } from "components/Reviews";
-  
-import { Products } from "pages/Products";
-  import { ProductDetails } from "pages/ProductDetails";
-
+import { SharedLayout } from "./SharedLayout";
+  import { Home } from "pages/Home";
+  import { About } from "pages/About";
+    import { Mission } from "components/Mission";
+    import { Team } from "components/Team";
+    import { Reviews } from "components/Reviews";
+  import { Products } from "pages/Products";
+    import { ProductDetails } from "pages/ProductDetails";
 import { NotFound } from "pages/NotFound";
+  
+// import { Container, Header, Logo, Link } from "./SharedLayout.styled";
 
-import { Container, Header, Logo, Link } from "./App.styled";
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{" "}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> //! */}
-        <Route path="/about" element={<About />}>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-
-        <Route path="/products" element={<Products />} />
-
-        <Route path="/products/:id" element={<ProductDetails />} />
-
-        <Route path="*" element={<NotFound />} />
-        
-      </Routes>
-    </Container>
+        <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
+
+
+
+
+//todo  ==> до Индексные маршруты
+// export const App = () => {
+//   return (
+//     <Container>
+
+//       <Header>
+//         <Logo>
+//           <span role="img" aria-label="computer icon">
+//             💻
+//           </span>{" "}
+//           GoMerch Store
+//         </Logo>
+
+//         <nav>
+//           <Link to="/">Home</Link>
+//           <Link to="/about">About</Link>
+//           <Link to="/products">Products</Link>
+//         </nav>
+//       </Header>
+      
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+
+//         {/* <Route path="/about" element={<About />} /> //! */}
+//         <Route path="/about" element={<About />}>
+//           <Route path="mission" element={<Mission />} />
+//           <Route path="team" element={<Team />} />
+//           <Route path="reviews" element={<Reviews />} />
+//         </Route>
+
+//         <Route path="/products" element={<Products />} />
+
+//         <Route path="/products/:id" element={<ProductDetails />} />
+
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </Container>
+//   );
+// };
