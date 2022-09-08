f6ace79df07a3d32ed36dfc56af29f5589e8da78
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-09-08 14:01:03 +0300

    Start API themoviedb.org 15

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,8 +16,8 @@ export const App = () => {
   const BASE_URL = 'https://pixabay.com/api/';
   const page = 1;
   const per_page = 12;
-  // const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&page=${page}&per_page=${per_page}`; //! with API_KEY
-
+  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&page=${page}&per_page=${per_page}`; //! with API_KEY
+  console.log("url: ", url);
   //! pixabay.com
   // async function fetchPixabay() {
   //   const response = await axios.get(url)
@@ -38,7 +38,7 @@ export const App = () => {
 
 
 
-//! pixabay.com
+//! themoviedb.org
 async function fetchPixabay() {
     const response = await axios.get("https://api.themoviedb.org/3/movie/550?api_key=41b230c5977baa736e324532e16fdadb")
     const all = await response.data
